05ab2e2ceb6337231c8efc816a259e7d138b9d25
Dhruvish09 <pateldhruvish612000@gmail.com>
2021-06-07 20:40:05 +0530

    up

--- pro/settings.py
+++ pro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'yt-l+ho(+*sujj3(thgk8anh3&dwjto_!&=ozu!gp)2@6ek)pq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
